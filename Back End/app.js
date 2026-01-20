d059f93017c76b98616796c3b8ca73883e17c0f0
Abdullah Sawan <abdullahsawan19@gmail.com>
2026-01-21 01:14:04 +0200

    Global Error Handeler

--- Back End/app.js
+++ Back End/app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
+const globalErrorHandler = require("./controllers/error.Controller");
+const AppError = require("./utils/appError");
 
 const app = express();
-
-// Middleware
-app.use(express.json());
 
 // Logger (only in development)
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
+
+// Routes
+
+// Middleware
+app.use(express.json());
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl}`, 404));
+});
+
+app.use(globalErrorHandler);
+
 module.exports = app;
--- Back End/index.js
+++ Back End/index.js
@@ -1,4 +1,11 @@
 const dotenv = require("dotenv");
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: ".env" });
 const app = require("./app");
 const connectDB = require("./config/db.config");
@@ -11,4 +18,11 @@ const server = app.listen(PORT, () => {
   console.log(
     `🚀 Server running on port ${PORT} in ${process.env.NODE_ENV} mode`,
   );
+});
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
