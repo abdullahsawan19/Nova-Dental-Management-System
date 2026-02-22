9652ba3f6083efc0201a17950e0ff96036bfc917
Abdullah Sawan <abdullahsawan19@gmail.com>
2026-02-22 18:48:22 +0200

    fix: Improve database connection handling and logging in db.config.js

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,14 +1,22 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("✅ Using existing Database connection");
+    return;
+  }
+
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URL);
-    console.log("✅✅✅✅✅  Database Connected");
+    const db = await mongoose.connect(process.env.MONGO_URL);
+
+    isConnected = db.connections[0].readyState;
+    console.log("✅✅✅✅✅  Database Connected Successfully");
   } catch (error) {
-    console.log(
-      `❎❎❎❎❎  Database connection errorrrrrrrrrrr ${error.message}`,
-    );
-    process.exit(1);
+    console.log(`❎❎❎❎❎  Database connection error: ${error.message}`);
   }
 };
 
