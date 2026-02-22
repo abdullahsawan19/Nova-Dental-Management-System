daf8a45c785b231867dc0984c23b88f0cda0afc4
Abdullah Sawan <abdullahsawan19@gmail.com>
2026-02-22 20:17:43 +0200

    edit db.config.js

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,22 +1,22 @@
 const mongoose = require("mongoose");
 
-let isConnected = false;
-
 const connectDB = async () => {
-  mongoose.set("strictQuery", true);
+  const URI =
+    "mongodb+srv://abdullahsawan19_db_user:uBxCTc3ptDBHYQmR@graduationproject0.5iwlj4n.mongodb.net/clinic?retryWrites=true&w=majority";
 
-  if (isConnected) {
-    console.log("✅ Using existing Database connection");
+  if (mongoose.connection.readyState >= 1) {
+    console.log("✅ Already connected to MongoDB");
     return;
   }
 
   try {
-    const db = await mongoose.connect(process.env.MONGO_URL);
-
-    isConnected = db.connections[0].readyState;
-    console.log("✅✅✅✅✅  Database Connected Successfully");
+    await mongoose.connect(URI, {
+      serverSelectionTimeoutMS: 5000,
+      bufferCommands: false,
+    });
+    console.log("✅✅✅✅✅ Database Connected Successfully on Vercel");
   } catch (error) {
-    console.log(`❎❎❎❎❎  Database connection error: ${error.message}`);
+    console.log(`❎❎❎❎❎ Database connection error: ${error.message}`);
   }
 };
 
