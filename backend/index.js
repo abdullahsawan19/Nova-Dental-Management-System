05582942cadc8b5cee629baf0c6d9389789bb977
Abdullah Sawan <abdullahsawan19@gmail.com>
2026-02-22 17:26:23 +0200

    fix: Refactor server initialization and unhandled rejection handling in index.js

--- backend/index.js
+++ backend/index.js
@@ -12,19 +12,21 @@ const connectDB = require("./config/db.config");
 
 connectDB();
 
-const PORT = process.env.PORT;
-
-const server = app.listen(PORT, () => {
-  console.log(
-    `🚀 Server running on port ${PORT} in ${process.env.NODE_ENV} mode`,
-  );
-});
-process.on("unhandledRejection", (err) => {
-  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  const server = app.listen(PORT, () => {
+    console.log(
+      `🚀 Server running on port ${PORT} in ${process.env.NODE_ENV} mode`,
+    );
   });
-});
+
+  process.on("unhandledRejection", (err) => {
+    console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+    console.log(err.name, err.message);
+    server.close(() => {
+      process.exit(1);
+    });
+  });
+}
 
 module.exports = app;
