0074c28ceedb57c266b5dd8630c5772f126cfb28
Abdullah Sawan <abdullahsawan19@gmail.com>
2026-02-22 20:25:55 +0200

    edit index.js

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,11 @@
 const dotenv = require("dotenv");
-
-process.on("uncaughtException", (err) => {
-  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
-  console.log(err.name, err.message);
-  process.exit(1);
-});
-
 dotenv.config({ path: ".env" });
+
 const app = require("./app");
 const connectDB = require("./config/db.config");
 
-connectDB();
+module.exports = async (req, res) => {
+  await connectDB();
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  const server = app.listen(PORT, () => {
-    console.log(
-      `🚀 Server running on port ${PORT} in ${process.env.NODE_ENV} mode`,
-    );
-  });
-
-  process.on("unhandledRejection", (err) => {
-    console.log("UNHANDLED REJECTION! 💥 Shutting down...");
-    console.log(err.name, err.message);
-    server.close(() => {
-      process.exit(1);
-    });
-  });
-}
-
-module.exports = app;
+  return app(req, res);
+};
