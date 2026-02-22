d7626216071bfcc811165b98e17b0032ea576dd3
Abdullah Sawan <abdullahsawan19@gmail.com>
2026-02-22 19:10:38 +0200

    add trust proxy in app.js

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,8 @@ const appointmentController = require("./controllers/appointment.Controller");
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 app.use(
   cors({
     origin: process.env.ALLOWED_ORIGINS,
